d38f9f6fd764d64b04a880786e40a0c48ced28fd
praveenkej <85007428+praveenkej@users.noreply.github.com>
2023-12-04 18:18:39 +0530

    Add files via upload

---
+++ Meme Bazaar/filter.js
@@ -0,0 +1,495 @@
+const data = [
+  {
+    id: 1,
+    name: "Miracle Miracle",
+    img: "https://indian.memetemplates.in/uploads/1639246771.jpeg",
+ 
+    cat: "Akshay Kumar",
+  },
+  {
+    id: 11,
+    name: "Control Uday Control",
+    img: "https://indian.memetemplates.in/uploads/1639680131.jpeg",
+ 
+    cat: "Welcome",
+  },
+  {
+    id: 2,
+    name: "Bas Kar Bhai",
+    img: "https://indian.memetemplates.in/uploads/1639258647.jpeg",
+   
+    cat: "Other",
+  },
+  {
+    id: 3,
+    name: "Mera Hi Maal Chura Kar ",
+    img: "https://indian.memetemplates.in/uploads/1631275058.jpg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 4,
+    name: "Taste Kha Hai",
+    img: "https://indian.memetemplates.in/uploads/1638446738.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Pyaari Samajh Gyi",
+    img: "https://indian.memetemplates.in/uploads/1638815488.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Ashleel Hai Ye launda",
+    img: "https://indian.memetemplates.in/uploads/1638987397.jpeg",
+    
+    cat: "3 Idiots",
+  },
+ 
+  {
+    id: 5,
+    name: "Me Expert Hu",
+    img: "https://indian.memetemplates.in/uploads/1639767173.jpeg" ,
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Ye Bhi Thik Hai ",
+    img: "https://indian.memetemplates.in/uploads/1639334677.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Itni khushi Mujhe",
+    img: "https://indian.memetemplates.in/uploads/1639514260.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Wo Raat Apun Do Baje",
+    img: "https://indian.memetemplates.in/uploads/1639165391.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Dono Alag Alag Hote Hai Kya",
+    img: "https://indian.memetemplates.in/uploads/1638734277.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Neeche Se Check Kar ",
+    img: "https://indian.memetemplates.in/uploads/1638988482.jpeg",
+    
+    cat: "3 Idiot",
+  },
+
+  {
+    id: 5,
+    name: "Kya Kru Me Mar Jau",
+    img: "https://indian.memetemplates.in/uploads/1639597257.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Samajh Nhi Aaya Par ",
+    img: "https://indian.memetemplates.in/uploads/1639854694.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Jali Na Teri BHi Jali Na",
+    img: "https://indian.memetemplates.in/uploads/1638988482.jpeg",
+    
+    cat: "Akshay Kumar",
+  },
+  {
+    id: 5,
+    name: "Mujhe Ghar Jaana Hai",
+    img: "https://indian.memetemplates.in/uploads/1640017167.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Abba Nhi Manenge ",
+    img: "https://indian.memetemplates.in/uploads/1640191647.jpeg",
+    
+    cat: "3 Idiots",
+  },
+  {
+    id: 5,
+    name: "Beta Tumse Na Ho Payega",
+    img: "https://indian.memetemplates.in/uploads/1641410719.jpeg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Mera Toh Itna Life Kharab Ho",
+    img: "https://indian.memetemplates.in/uploads/1640026893.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Seh Lenge Thoda",
+    img: "https://indian.memetemplates.in/uploads/1641321299.jpeg",
+    
+    cat: "Welcome",
+  },
+  {
+    id: 5,
+    name: "Haan Ye Krlo Pehle",
+    img: "https://indian.memetemplates.in/uploads/1641492919.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "O Bhai Maaro Mujhe Maaro ",
+    img: "https://indian.memetemplates.in/uploads/1641747886.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Mujhe Kyo Toda",
+    img: "https://indian.memetemplates.in/uploads/1642099582.jpeg",
+    
+    cat: "Welcome",
+  },
+  {
+    id: 5,
+    name: "Paisa Laya",
+    img: "https://indian.memetemplates.in/uploads/1642260090.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Abhi Maza Aayega Na",
+    img: "https://indian.memetemplates.in/uploads/1674731583.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Neeche Se Check Kar ",
+    img: "https://indian.memetemplates.in/uploads/1638988482.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Dekh Rha Hai Binod ",
+    img: "https://indian.memetemplates.in/uploads/1675096256.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "Jal Lijiye",
+    img: "https://indian.memetemplates.in/uploads/1642460335.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Bhai Kya Kar Rha Hai Tu ",
+    img: "https://indian.memetemplates.in/uploads/1675005298.jpeg",
+    
+    cat: "Other",
+  },
+  {
+    id: 5,
+    name: "It's Not Funny",
+    img: "https://indian.memetemplates.in/uploads/1675352238.jpg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Chhoti Bacchi Ho Kya",
+    img: "https://indian.memetemplates.in/uploads/1676051197.jpeg",
+    
+    cat: "Bollywood",
+  },
+
+  {
+    id: 5,
+    name: "Saala Ye Dhukh",
+    img: "https://indian.memetemplates.in/uploads/1676393448.jpeg",
+    
+    cat: "Bollywood",
+  },
+  {
+    id: 5,
+    name: "Tere Baap Ki Saadi ",
+    img: "https://indian.memetemplates.in/uploads/1676479333.jpeg",
+    
+    cat: "Hera Pheri",
+  },
+  {
+    id: 5,
+    name: "Jalwa Hai Humara",
+    img: "https://indian.memetemplates.in/uploads/1677415388.jpeg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Ye Badhiya Tha Guru ",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ye-badhiya-tha-guru-1200x674.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Aapse Better Umeed Kiye The",
+    img: "https://images.assettype.com/nationalherald/2021-06/bea94fe5-f339-4cec-8df6-e4463ab68927/Sehwag_s_Mirzapur_meme_after_India_loss_in_WTC_final_goes_viral.jpg",
+    
+    cat: "Mirzapur",
+  },
+  {
+    id: 5,
+    name: "Buddhi Bahut Tez Hai Tumhari",
+    img: "https://bharatiyavishwa.com/wp-content/uploads/2020/06/buddhi-bohut-tej-hai-tumhari.jpg",
+    
+    cat: "Mirzapur",
+  },
+
+
+
+
+
+  {
+    id: 5,
+    name: "Arey Band Kar... Band Kar  ",
+    img: "https://www.india.com/wp-content/uploads/2022/09/taarak-mehta-ooltah-chashmah-memes.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Goli Beta Masti Nhi",
+    img: "https://indianmemetemplates.com/wp-content/uploads/goli-beta-masti-nahi.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Nahane Ja Nahane",
+    img: "https://memetemplates.in/uploads/1628243703.jpeg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Kya Tapleek Hai Aapko",
+    img: "https://memes.co.in/memes/update/uploads/2021/02/kya-tapleek-hai-aap-ko.jpg",
+    
+    cat: "Taarak Mehta",
+  },{
+    id: 5,
+    name: "Happy Diwali",
+    img: "https://humornama.com/wp-content/uploads/2021/11/Happy-Diwali-Meme-on-Jethalal-608x608.jpg",
+    
+    cat: "Taarak Mehta",
+  },
+  {
+    id: 5,
+    name: "Chabi Kha Hai",
+    img: "https://indianmemetemplates.com/wp-content/uploads/chabi-kaha-hai-1200x675.jpg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Gaadi Start Kar",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ae-gaadi-start-kar-1200x675.jpg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Saanp Ko Paal Rha Tha",
+    img: "https://indianmemetemplates.com/wp-content/uploads/saala-saanp-ko-paal-raha-tha.jpg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Achha Baat Nhi Hai",
+    img: "https://indianmemetemplates.com/wp-content/uploads/accha-baat-nahi-hai-ye-1200x674.jpg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Aapka Hai Toj Kaagaz Dikhaiye",
+    img: "https://indianmemetemplates.com/wp-content/uploads/aapka-hai-toh-kagaz-dikhaiye.jpg",
+    
+    cat: "Wasseypur",
+  },
+  {
+    id: 5,
+    name: "Bhai Paisa Ho Toh Kya ",
+    img: "https://memes.co.in/memes/update/uploads/2021/12/kbidhnd3wj671-950x500.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Do Laana",
+    img: "https://akm-img-a-in.tosshub.com/sites/lallantop/wp-content/uploads/2022/03/whatsapp-image-2022-03-16-at-33908-pm_160322-100925.jpeg",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Saale Ko Bomb Se Uda Dunga",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrCjqZ6Qi20YguKFYm7cuW-G9fkAys_uOpIuo9EINjR-S1X9vEuBIcK4ueSxDgKgyCewg&usqp=CAU",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Thik Hai Bhai Me Chalta Hu ",
+    img: "https://indianmemetemplates.com/wp-content/uploads/thik-hai-bhai-ab-mai-chalta-hoon-1200x600.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+  {
+    id: 5,
+    name: "Ye Kab Hua",
+    img: "https://indianmemetemplates.com/wp-content/uploads/ye-kab-hua-1200x674.jpg",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Hum Koi Mandir Ka Ghanta",
+    img: "https://i0.wp.com/adthejokester.com/wp-content/uploads/2021/09/Hum-koi-mandir-ka-Ghanta-hai-kya-meme-template.jpg?resize=860%2C484&ssl=1",
+    
+    cat: "Rajpal Yadav",
+  },
+
+  {
+    id: 5,
+    name: "Aurat Ka Chakkar Babu Bhaiya",
+    img: "https://indianmemetemplates.com/wp-content/uploads/Aurat-Ka-Chakkar-Babu-Bhaiya-800x600.jpg?crop=1",
+    
+    cat: "Akshay Kumar",
+  },
+  {
+    id: 5,
+    name: "Kya Maal Hai Yaar",
+    img: "https://i.pinimg.com/736x/5d/62/e6/5d62e63e30d893287e447b27596d13c5.jpg",
+    
+    cat: "Akshay Kumar",
+  },
+
+];
+const downloadImg = (imgURL) =>{
+fetch(imgURL).then(res =>res.blob()).then(file =>{
+console.log(file)
+}).catch(()=>alert("Failed to download image!"));
+}
+
+const  showlightbox = (img) =>{
+  lightBox.querySelector("img").src=img;
+  lightBox.classList.add("show");
+}
+const hidelightBox = () =>{
+  lightBox.classList.remove("show");
+}
+const productsContainer = document.querySelector(".products");
+const searchInput = document.querySelector(".search");
+const lightBox= document.querySelector(".lightbox");
+const close= lightBox.querySelector(".clse");
+const categoriesContainer = document.querySelector(".cats");
+const displayProducts = (filteredProducts) => {
+  productsContainer.innerHTML = filteredProducts
+    .map(
+     
+
+      (product) =>
+        `
+       <div onclick="showlightbox('${product.img}')">
+       <div class="product">
+       <div class="hoverimage">
+       <img
+          src=${product.img}
+          alt=""
+          />
+          </div>
+    
+          <span class="name">${product.name}</span>
+          <span class="download">${product.cat}</span>
+          
+        </button>
+
+        </div>
+        </div>
+    `
+    )
+    .join("");
+};
+
+displayProducts(data);
+
+searchInput.addEventListener("keyup", (e) => {
+  const value = e.target.value.toLowerCase();
+
+  if (value) {
+    displayProducts(
+      data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+    );
+  } else {
+    displayProducts(data);
+  }
+});
+
+const setCategories = () => {
+  const allCats = data.map((item) => item.cat);
+  const categories = [
+    "All",
+    ...allCats.filter((item, i) => {
+      return allCats.indexOf(item) === i;
+    }),
+  ];
+
+  categoriesContainer.innerHTML = categories
+    .map(
+      (cat) =>
+        `
+      <span class="cat">${cat}</span>
+    `
+    )
+    .join("");
+
+  categoriesContainer.addEventListener("click", (e) => {
+    const selectedCat = e.target.textContent;
+
+    selectedCat === "All"
+      ? displayProducts(data)
+      : displayProducts(data.filter((item) => item.cat === selectedCat));
+  });
+};
+
+
+
+setCategories();
+close.addEventListener("click",hidelightBox)
+
+
